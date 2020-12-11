e40b43529e5c76bf40d7d7a94bf43d7b508652a9
chenxuanguang <chenxuanguang@chuangxin.com>
2020-12-11 17:53:42 +0800

    添加 tags 页面和对应的 css

--- _site/org/auto/tikz_demo.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook
- "tikz_demo"
- (lambda ()
-   (TeX-run-style-hooks
-    "latex2e"
-    "minimal"
-    "minimal10"
-    "tikz")))
-
---
+++ readme.md
@@ -0,0 +1,5 @@
+== 个人博客
+
+== 启动方式
+
+bundle exec jekyll serve
